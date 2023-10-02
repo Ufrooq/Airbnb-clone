58b36840022dce1b0d3cd1a44487951e5294291b
Umar <muhammadshafqatali3@gmail.com>
2023-10-02 17:43:19 +0500

    front-end-footer-part

--- client-side/src/components/footer/Footer.js
+++ client-side/src/components/footer/Footer.js
@@ -4,8 +4,43 @@ import "./style.scss";
 const Footer = () => {
   return (
     <section className="foot">
-      <h3>airbnb</h3>
-      <p>copyright@11.1</p>
+      <ul className="left-section">
+        <li>
+          <h3>© 2023 Airbnb, Inc.</h3>
+        </li>
+        .
+        <li>
+          <a href="">Terms</a>
+        </li>
+        .
+        <li>
+          <a href="">Privacy</a>
+        </li>
+        .
+        <li>
+          <a href="">Sitemap</a>
+        </li>
+      </ul>
+      <ul className="right-section">
+        <li>
+          <h3>$USD</h3>
+        </li>
+        <li>
+          <a href="">
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i class="fa-brands fa-square-x-twitter"></i>
+          </a>
+        </li>
+      </ul>
     </section>
   );
 };
