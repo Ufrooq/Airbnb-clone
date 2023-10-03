203521a3c0b55a03090d1f3664702299de6ce132
Umar <muhammadshafqatali3@gmail.com>
2023-10-04 02:28:59 +0500

    front-end-room-part

--- client-side/src/components/rooms/Rooms.js
+++ client-side/src/components/rooms/Rooms.js
@@ -47,6 +47,18 @@ const Rooms = () => {
           />
         </div>
       </div>
+      <div className="rooms_info_section">
+        <div className="room_charges_card">
+          <div className="top_Ofcard">
+            <p>
+              <span>$26</span> night
+            </p>
+            <p>
+              <i class="fa-solid fa-star"></i>4.9 . <a href="/">56 reviews</a> .
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
