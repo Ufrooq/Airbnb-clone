f11558333c4ad2c392ee873bcedbc227b3948f1d
Umar <muhammadshafqatali3@gmail.com>
2023-10-11 22:47:07 +0500

    dsdfsdfs

--- server-side/index.js
+++ server-side/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRoutes from "./Routes/userRoutes.js";
 import cors from "cors";
+import { dbCon } from "./Connections/connectDb.js";
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -8,6 +9,9 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use("/", userRoutes);
+
+// dbconnection --->
+dbCon();
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
