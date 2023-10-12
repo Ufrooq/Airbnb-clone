e0f15dd0bd337439ebabb75c98e2c712af050fca
Umar <muhammadshafqatali3@gmail.com>
2023-10-12 18:45:50 +0500

    dsdfsdfs

--- server-side/Controllers/userControllers.js
+++ server-side/Controllers/userControllers.js
@@ -1,7 +1,13 @@
+import { userModel } from "../Models/userModel.js";
+import bcrypt from "bcrypt";
+
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    console.log(name, email, password);
+    const isUserExist = await userModel.findOne({ email });
+    if (isUserExist) {
+      res.status(409).json({ message: "username already used" });
+    }
     res.status(200).json("okokkokokok");
   } catch (error) {
     res.status(500).json(error);
---
+++ server-side/Models/userModel.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+export const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const userModel = new model("users", userSchema);
