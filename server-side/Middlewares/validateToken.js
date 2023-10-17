4d57f97ea23109a32f1db6a2bdea99e56463afff
Umar <muhammadshafqatali3@gmail.com>
2023-10-17 23:59:15 +0500

    dsdsdas

---
+++ server-side/Middlewares/validateToken.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import userModel from "../Models/userModels";
+
+dotenv.config();
+
+export const checkCurrentUser = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    jwt.verify(token, process.env.EXCESS_TOKEN, async (error, decoded) => {
+      if (error) {
+        console.log(error.message);
+      } else {
+        try {
+          const currentUser = await userModel.findOne(decoded.id);
+        } catch (error) {}
+      }
+    });
+  }
+};
