098d0753a23e19eb45360fc57f60ce5279405f22
Umar <muhammadshafqatali3@gmail.com>
2023-11-18 21:20:00 +0500

    profile-component

--- server-side/Routes/userRoutes.js
+++ server-side/Routes/userRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../Controllers/userControllers.js";
+import { checkCurrentUser } from "../Middlewares/validateToken.js";
 
 const router = Router();
 
+app.get("*", checkCurrentUser);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 // router.post("/avatar", validateUser, setAvatar);
