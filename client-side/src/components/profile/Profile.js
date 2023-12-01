ca6cf1245949b5c2adea3e2c9f4d9cc24a0a042d
Umar <muhammadshafqatali3@gmail.com>
2023-12-01 19:47:32 +0500

    sdsda

--- client-side/src/components/profile/Profile.js
+++ client-side/src/components/profile/Profile.js
@@ -1,7 +1,26 @@
 import React from "react";
+import avatar from "../../assets/avatar.png";
+import "./style.scss";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <section className="profile_section">
+      <div className="profile_card">
+        <div className="personal_info">
+          <img src={avatar} alt="" />
+          <h2 className="name">Umar Farooq</h2>
+          <p className="details">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+            cupiditate.
+          </p>
+        </div>
+        <div className="date_time">
+          <h2>1</h2>
+          <p>month on airbnb</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
